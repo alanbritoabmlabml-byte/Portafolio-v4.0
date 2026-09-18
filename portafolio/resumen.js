94caad5d2d7620145ebc98fabbbd35afc875e3fe
alanbritoabmlabml-byte <alanbrito.abml.abml@gmail.com>
2026-09-18 16:37:17 -0400

    Add files via upload

---
+++ portafolio/resumen.js
@@ -0,0 +1,2 @@
+/* Cifras de cabecera del Portafolio, para la portada. Se regenera junto con datos.js. */
+window.PC_RESUMEN = {"fecha": "2026-09-18", "iniciativas": 162, "actividades": 361, "conPlan": 59, "cerradasIni": 20, "actCerradas": 236, "actVencidas": 49, "avanceMedio": 69, "areas": 21, "responsables": 26};
